2c42436fdf0be2fe712c5c7168a3ec35a64e8723
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-09-03 15:07:26 -0300

    Update app.routes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -11,4 +11,16 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'user-info',
+    loadComponent: () => import('./Pages/admin/user-info/user-info.page').then( m => m.UserInfoPage)
+  },
+  {
+    path: 'user-links',
+    loadComponent: () => import('./Pages/admin/user-links/user-links.page').then( m => m.UserLinksPage)
+  },
+  {
+    path: 'theme-management',
+    loadComponent: () => import('./Pages/admin/theme-management/theme-management.page').then( m => m.ThemeManagementPage)
+  },
 ];

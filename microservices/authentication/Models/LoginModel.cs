d04c3b0566bb4c5957bb56d8d6ecdf5650e974a1
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-09-05 10:45:07 -0300

    Update LoginModel.cs

--- microservices/authentication/Models/LoginModel.cs
+++ microservices/authentication/Models/LoginModel.cs
@@ -4,15 +4,16 @@ namespace authentication.Models
 {
     public class LoginModel
     {
-        public LoginModel(string name, string username, string email, string password)
+        public LoginModel() { }
+        public LoginModel(string? username, string? email, string password)
         {
-            Username = username;
-            Email = email;
-            Password = password;
+            username = username;
+            email = email;
+            password = password;
         }
         
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string? username { get; set; }
+        public string? email { get; set; }
+        public string password { get; set; }
     }
 }

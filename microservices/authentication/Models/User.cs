9808ec6541f6b612644fe97389aeb460372b6752
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-09-05 10:45:12 -0300

    Update User.cs

--- microservices/authentication/Models/User.cs
+++ microservices/authentication/Models/User.cs
@@ -3,18 +3,21 @@
     public class User
     {
         public User() { }
-        public User(int id, string name, string username, string email, string password)
+        public User(int id, string name, string username, string email, string password, DateTime createdat, DateTime updatedat)
         {
-            Id = id;
-            Username = username;
-            Email = email;
-            Password = password;
+            id = id;
+            username = username;
+            email = email;
+            password = password;
+            createdat = createdat;
+            updatedat = updatedat;
         }
 
-        public int Id { get; set; }
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public int id { get; set; }
+        public string? username { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+        public DateTime? createdat { get; set; }
+        public DateTime? updatedat { get; set; }
     }
 }

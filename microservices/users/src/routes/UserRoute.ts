20f196a7c7b7487046cd8acb6410adf6b133f17d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-09-05 16:12:21 -0300

    Update UserRoute.ts

--- microservices/users/src/routes/UserRoute.ts
+++ microservices/users/src/routes/UserRoute.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import UserController from '../controllers/UserController';
 
+import { authMiddleware, ownerMiddleware } from '../middlewares/authMiddleware';
+
 const controller = UserController
 
 const router = Router();
@@ -10,7 +12,7 @@ router.post('/users', controller.create);
 router.get('/users', controller.get);
 router.get('/users/:id', controller.getById);
 router.get('/users/search/:field', controller.getOne);
-router.put('/users/:id', controller.update);
-router.delete('/users/:id', controller.delete);
+router.put('/users/:id', authMiddleware, ownerMiddleware, controller.update);
+router.delete('/users/:id', authMiddleware, ownerMiddleware, controller.delete);
 
 export default router

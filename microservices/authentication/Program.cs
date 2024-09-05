4944bb53b593bc3bb6ec6db47bfe2cfadddc71d5
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-09-05 10:45:18 -0300

    Update Program.cs

--- microservices/authentication/Program.cs
+++ microservices/authentication/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
